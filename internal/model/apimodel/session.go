34e38a8ceda47f93c1d1e572beb76c9d77056df0
Dmitriy Kazakov <dkazakov@ivi.ru>
2025-11-20 14:52:59 +0300

    Поправил логику формирования оценок, если карты скрыты, отправляем скрытые значения, если все проголосовали реальные оценки

--- internal/model/apimodel/session.go
+++ internal/model/apimodel/session.go
@@ -6,18 +6,20 @@ import (
 )
 
 type Session struct {
-	ID            uuid.UUID            `json:"id"`
-	Name          string               `json:"name"`
-	DeckType      model.DeckType       `json:"deck_type"`
-	CardsRevealed bool                 `json:"cards_revealed"`
-	CreatorID     uuid.UUID            `json:"creator_id"`
-	CreatorName   string               `json:"creator_name"`
-	AutoReveal    bool                 `json:"auto_reveal"`
-	AllowEmoji    bool                 `json:"allow_emoji"`
-	CreatedVia    string               `json:"created_via"`
-	DeckValues    []string             `json:"deck_values"`
-	Users         []UsersInSession     `json:"users"`
-	Votes         map[uuid.UUID]string `json:"votes"`
+	ID             uuid.UUID            `json:"id"`
+	Name           string               `json:"name"`
+	DeckType       model.DeckType       `json:"deck_type"`
+	CardsRevealed  bool                 `json:"cards_revealed"`
+	CreatorID      uuid.UUID            `json:"creator_id"`
+	CreatorName    string               `json:"creator_name"`
+	AutoReveal     bool                 `json:"auto_reveal"`
+	AllowEmoji     bool                 `json:"allow_emoji"`
+	CreatedVia     string               `json:"created_via"`
+	DeckValues     []string             `json:"deck_values"`
+	Users          []UsersInSession     `json:"users"`
+	Votes          map[uuid.UUID]string `json:"votes"`
+	AverageVote    *string              `json:"average_vote"`
+	MostCommonVote *string              `json:"most_common_vote"`
 }
 
 type UsersInSession struct {
